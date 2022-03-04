1424903584df3ff2a346ab83581299a9ffc0c403
ztandrews <68918006+ztandrews@users.noreply.github.com>
2022-03-04 15:57:44 -0500

    Added root API route

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -5,8 +5,13 @@ from schemas.schemas import user_serializer, users_serializer
 
 user_api_router = APIRouter()
 
-# get
+#Greeting message
 @user_api_router.get("/")
+async def welcome():
+    return {"message":"Welcome to the BetShare API"}
+
+
+@user_api_router.get("/users")
 async def get_users():
     users = users_serializer(users_collection.find())
     return {"status":"ok", "data":users}
