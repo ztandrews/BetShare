87896cd45626fbf550ec773d9b081fb2877ae59f
ztandrews <68918006+ztandrews@users.noreply.github.com>
2022-03-06 11:39:27 -0500

    API Calls Work Now

--- frontend/src/components/Discover.js
+++ frontend/src/components/Discover.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Discover extends Component {
+    state= {
+        users: []
+    }
+
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000/users").then(res => {
+            const users = res.data.data;
+            console.log(users);
+            this.setState({users: users});
+        })
+    }
+
     render() {
         return (
             <div className = "container">
                 <h1 className = "page-header">Discover</h1>
+                <ul>
+                    {
+                        this.state.users
+                        .map(user => 
+                            <li key={user.id}>{user.name}</li>
+                            )
+                    }
+                </ul>
             </div>
         )
     }
