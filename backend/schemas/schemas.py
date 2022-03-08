cb9a3b69debf09f46a00df93fab9fa5961260565
ztandrews <68918006+ztandrews@users.noreply.github.com>
2022-03-08 13:55:03 -0500

    API Work

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -23,7 +23,7 @@ def users_serializer(users) -> list:
 def bet_serializer(bet) -> dict:
     user_dict = bet["user"][0]
     team_for_dict = bet["team_for"][0]
-    #team_against_dict = bet["team_against"][0]
+    team_against_dict = bet["team_agaisnt"][0]
 
     return{
         "id":str(bet["_id"]),
@@ -35,7 +35,9 @@ def bet_serializer(bet) -> dict:
         "team_for":{"id":str(team_for_dict["_id"]),"city":team_for_dict["city"],"team":team_for_dict["team"],
         "abv":team_for_dict["abv"],"league":team_for_dict["league"]},
 
-        "team_against":str(bet["team_agaisnt"]),
+        "team_against":{"id":str(team_against_dict["_id"]),"city":team_against_dict["city"],"team":team_against_dict["team"],
+        "abv":team_against_dict["abv"],"league":team_against_dict["league"]},
+
         "amount":bet["amount"],
         "odds":bet["odds"],
         "outcome":bet["outcome"],
